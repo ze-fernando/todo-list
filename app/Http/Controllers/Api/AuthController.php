065a3bbd57ec5f75ca4169dd873c4e104fb2d4bb
ze-fernando <zefernando.dev@gmail.com>
2025-07-24 13:32:21 -0300

    project struc

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request){
+        $request->validate([
+            'username' => 'required|string|exists:users,name',
+            'password' => 'required|string|min:8'
+        ]);
+    }
 }
