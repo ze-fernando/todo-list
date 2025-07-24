354d78685291339b31a4e3bdffd51ff6c613b75f
ze-fernando <zefernando.dev@gmail.com>
2025-07-24 13:35:34 -0300

    feat: auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,14 +3,89 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
-        $request->validate([
-            'username' => 'required|string|exists:users,name',
-            'password' => 'required|string|min:8'
-        ]);
+    private function generateToken(User $user): string
+    {
+        return $user->createToken('auth_token')->plainTextToken;
+    }
+
+    private function deleteToken(User $user): void
+    {
+        $user->currentAccessToken()->delete();
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        try {
+            $data = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required|min:8'
+            ]);
+
+            if (!Auth::attempt($data)) {
+                return response()->json([
+                    'message' => 'Email e senha incorretos, verifique as credenciais ou crie um conta'
+                ], 401);
+            }
+
+            $user = Auth::user();
+
+            $token = $this->generateToken($user);
+
+            return response()->json(['user' => $user, 'access_token' => $token, 'token_type' => 'Bearer']);
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return response()->json(['message' => 'Ocorreu um erro ao tentar fazer login']);
+        }
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        try {
+            $data = $request->validate([
+                'name' => 'required',
+                'age' => 'required|numeric',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|confirmed|min:8',
+            ]);
+
+            $user = User::create($data);
+            $user->setFreePlan();
+
+            if ($user) {
+                $token = $this->generateToken($user);
+            }
+
+            return response()->json(['user' => $user, 'access_token' => $token, 'token_type' => 'Bearer']);
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return response()->json(['message' => 'Ocorreu um erro ao tentar criar sua conta']);
+        }
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        try {
+            $user = Auth::user();
+
+            $this->deleteToken($user);
+
+            return response()->json([
+                'message' => 'Logout realizado com sucesso'
+            ]);
+        } catch (\Throwable $th) {
+            Log::error('Erro no logout: ' . $th->getMessage());
+
+            return response()->json([
+                'message' => 'Erro ao tentar fazer logout'
+            ], 500);
+        }
     }
 }
