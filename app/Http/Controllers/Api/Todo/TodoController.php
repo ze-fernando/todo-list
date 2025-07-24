1398459b955e653eb5966591d5cede830ea235c2
ze-fernando <zefernando.dev@gmail.com>
2025-07-24 13:53:50 -0300

    feat: todo controller

---
+++ app/Http/Controllers/Api/Todo/TodoController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use PhpParser\Node\Stmt\TryCatch;
+
+class TodoController extends Controller
+{
+    public function index(){
+        try {
+            $id = Auth::id();
+
+            $todos = Todo::where('user_id', $id)->get();
+
+            return response()->json($todos, 200);
+        } catch (\Throwable $th) {
+            Log::info('Erro ao listar todos ' . $th->getMessage());
+
+            return response()->json(['Erro ao listar todos ' => $th->getMessage()], 500);
+        }
+    }
+
+    public function show(int $id){
+        try {
+            $todo = Todo::find($id);
+
+            return response()->json($todo, 200);
+        } catch (\Throwable $th) {
+            Log::info('Erro ao listar todos ' . $th->getMessage());
+
+            return response()->json(['Erro ao listar todos ' => $th->getMessage()], 500);
+        }
+    }
+
+    public function store(Request $request){
+        try {
+            $data = $request->validate([
+                'title' => 'required|string|unique|todos,title',
+                'expire' => 'required|date',
+                'done' => 'required|boolean',
+                'priority' => Rule::in('low', 'medium', 'high')
+            ]);
+
+            $todo = Todo::create($data);
+
+            return response()->json($todo, 201);
+
+        } catch (\Throwable $th) {
+            Log::info('Erro ao criar todo ' . $th->getMessage());
+
+            return response()->json(['Erro ao criar todo' => $th->getMessage()], 500);
+        }
+    }
+
+
+    public function update(int $id){
+        try {
+            $todo = Todo::find($id);
+
+            if(!$todo){
+                return response()->json(['Erro' => 'Todo não encontrada'],404);
+            }
+
+            $todo->makeDone();
+
+            return response()->json($todo, 200);
+        } catch (\Throwable $th) {
+            Log::info('Erro ao atualizar todo ' . $th->getMessage());
+
+            return response()->json(['Erro ao atualizar todo ' => $th->getMessage()], 500);
+        } 
+    }
+
+    public function destroy(int $id){
+        try {
+            $todo = Todo::find($id);
+
+            if(!$todo){
+                return response()->json(['Erro' => 'Todo não encontrada'],404);
+            }
+
+            $todo->delete();
+
+            return response()->json();
+        } catch (\Throwable $th) {
+            Log::info('Erro ao atualizar todo ' . $th->getMessage());
+
+            return response()->json(['Erro ao atualizar todo ' => $th->getMessage()], 500);
+        } 
+    }
+}
